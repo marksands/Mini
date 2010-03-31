d87f94f5c9a3df1e7e68f2478fa348cf77dddec3
Mark Sands <marksands07@gmail.com>
2010-03-30 21:48:40 -0500

    removing unused files

--- test/test_shortener.rb
+++ test/test_shortener.rb
@@ -1,24 +1,10 @@
-require 'rubygems'
-require 'sinatra/test/unit'
-require File.join(File.dirname(__FILE__), '..', 'min')
+require 'contest'
+require 'rack/test'
 
 class TestShortener < Test::Unit::TestCase
-  
-  configure do
-    set :views => File.join(File.dirname(__FILE__), '..', 'views')
+
+  test "should write a test" do
+    flunk "write a test or I'll kneecap you"
   end
 
-  def test_home_renders_content
-    get '/'
-    
-    assert_equal 200, @response.status
-    assert @response.body.length > 0
-  end
-  
-  def test_shorten_redirects_to_info
-    post '/', {:url => ' $( "#id" ) .highlightFade(3000); '}
-    assert_equal 302, @response.status
-    assert_match %r{/}, @response['Location']
-  end
-  
 end
